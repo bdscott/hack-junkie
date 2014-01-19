2427951a2dae52e2f652b885de0b4b752c404f32
Jon <jaeteemail@gmail.com>
2014-01-19 02:06:44 -0500

    added image uploading

--- app/Controller/CompetitionsController.php
+++ app/Controller/CompetitionsController.php
@@ -1,17 +1,25 @@
 <?php
 class CompetitionsController extends AppController {
-	
-		
-	public $uses = array('Application');
-	
-	
 
-	function index(){
-		
+	public $uses = array('Application');
+
+	function index() {
+
 	}
-	
-	function upload(){
-		
+
+	public function upload() {
+			$file = $this -> data["Competitions"]["file"];
+			if ($file["error"] === UPLOAD_ERR_OK) {
+				if (move_uploaded_file($file["tmp_name"], 'images\uploads\event-logos\\'.$file["name"])) {
+						echo "Image Upload Successful!<br>";
+					return true;
+				}
+			}
+			else {
+				echo "Invalid Image!";
+			}
+			return false;
+
 	}
-	
-}
+
+};
